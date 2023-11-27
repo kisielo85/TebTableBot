813c408638e58f8f76045ec8880ff5a58a40fb7a
Marcel Mikulski <100922591+Le-programer@users.noreply.github.com>
2023-11-27 12:17:21 +0100

    /alert - naprawiona pętla

--- Other/alertLekcjeLoop.js
+++ Other/alertLekcjeLoop.js
@@ -6,58 +6,71 @@ const tableData = require('./tableData.js')
  * @param {import('discord.js').Client} client
 */
 
+var test = 'August 19, 1975 11:19:00'
+
 module.exports = async (client, dm_list, tableData) => {
-
     let currentTime = new Date();
+
+    // aktualna lekcja
     let timeIndex = 0;
+    for(let period of Object.values(tableData.idList.periods)){
+        let [hours, minutes] = period.starttime.split(':');
 
-    for(let a of Object.keys(tableData.idList.periods)){
-
-        let [hours, minutes] = tableData.idList.periods[a].endtime.split(':');
-        hours = parseInt(hours); minutes = parseInt(minutes);
-
-        if(!(hours + (minutes / 100) <= currentTime.getHours() + (currentTime.getMinutes() / 100))){
-            timeIndex = parseInt(a);
-            break
+        if (currentTime.getHours()*60+currentTime.getMinutes() >= parseInt(hours)*60+parseInt(minutes)){
+            timeIndex=parseInt(period.id)
         }
+        else {break}
     }
-    
+
     while(true){
         currentTime = new Date();
 
         [hours, minutes] = tableData.idList.periods[timeIndex].endtime.split(':');
-        hours = parseInt(hours); minutes = parseInt(minutes);
+        hours = parseInt(hours); minutes = parseInt(minutes)
 
-        console.log(((hours - currentTime.getHours()) * 60 * 60 + (minutes - currentTime.getMinutes() - 5) * 60 - currentTime.getSeconds()), timeIndex)
+        // 5min przed końcem lekcji
+        var waitTime=(hours - currentTime.getHours()) * 60 * 60 + (minutes - currentTime.getMinutes() - 5) * 60 - currentTime.getSeconds()
+        if (waitTime<0){timeIndex++; continue}
+        console.log(waitTime, timeIndex)
         
         // jeśli są dzisiaj lekcje
         if(timeIndex < 10){
-            await simple.sleep(((hours - currentTime.getHours()) * 60 * 60 + (minutes - currentTime.getMinutes() - 5) * 60 - currentTime.getSeconds()) * 1000)
-                timeIndex = timeIndex + 1
-                for(let user_id of Object.keys(dm_list)){ // co usera z dmlist
-                    if(!dm_list[user_id].alert){continue}
-                    let message = ''
-                    let toSend
-                    
-                    for(let plan of dm_list[user_id].list){ // 
-                        for(const c of await tableData.getTable(plan.type, plan.id, true)){
-                            if(parseInt(c.uniperiod) != timeIndex){continue}
-                            let sent = []
-                            
-                            for(let g of c.groupnames){
-                                if(plan.groups && plan.groups.includes(g) && !sent.includes(c.groupnames)){
-                                    toSend = await client.users.fetch(user_id)
-                                    sent.push(c.groupnames)
-                                    message += `klasa **${tableData.idList[plan.type][plan.id].short}** grupa **${c.groupnames.join(', ')}** sala **${tableData.idList.classrooms[c.classroomids[0]].short}** o **${c.starttime}**` + "\n"
-                                }
+            await simple.sleep(waitTime * 1000)
+            timeIndex++
+            
+            // co usera z dmlist
+            for(let user_id of Object.keys(dm_list)){
+                if(!dm_list[user_id].alert){continue}
+                let message = ''
+                let toSend
+                
+                for(let plan of dm_list[user_id].list){
+                    for(const c of await tableData.getTable(plan.type, plan.id, true)){
+                        if(parseInt(c.uniperiod) != timeIndex){continue}
+                        let sent = []
+                        
+                        for(let g of c.groupnames){
+                            if(plan.groups && (plan.groups.includes(g) || g == '') && !sent.includes(c.groupnames)){
+                                toSend = await client.users.fetch(user_id)
+                                sent.push(c.groupnames)
+                                
+                                message+=`${tableData.idList[plan.type][plan.id].short}` //klasa
+                                if (g!=''){ message+=` (${plan.groups.join(', ')})`} //grupa
+                                message+=`:  ${tableData.idList.subjects[c.subjectid].name} ${tableData.idList.classrooms[c.classroomids[0]].short}   ${c.starttime} - ${c.endtime}\n` //przedmiot, klasa, godzina
+
+                                /*if(g == '')
+                                    message += `klasa **${tableData.idList[plan.type][plan.id].short}** sala **${tableData.idList.classrooms[c.classroomids[0]].short}** o **${c.starttime}**` + "\n"
+                                else
+                                    message += `klasa **${tableData.idList[plan.type][plan.id].short}** grupa **${c.groupnames.join(', ')}** sala **${tableData.idList.classrooms[c.classroomids[0]].short}** o **${c.starttime}**` + "\n"*/
                             }
                         }
                     }
-                    if(toSend)
-                        await toSend.send(message)                        
                 }
+                if(toSend)
+                    await toSend.send(message)                        
+            }
         }else{
-            console.log("beep")
+            console.log("czej do następnego dnia")
             timeIndex = 0
             await simple.sleep(((23 - currentTime.getHours()) * 60 + (60 - currentTime.getMinutes()) + 20) * 60 * 1000)
         }
