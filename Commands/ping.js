3883e80a72bf21b66a31238a22e436f767deae58
Marcel Mikulski <100922591+Le-programer@users.noreply.github.com>
2023-12-07 08:49:04 +0100

    Update ping.js
    mała poprawa

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,17 +1,10 @@
 /** @param {import('discord.js').Events.InteractionCreate | import('discord.js').Events.MessageCreate} msg */
 module.exports = async ({msg, client}) => {
-    if(msg.channel.type != 1)// sprawdza czy kanał to dm 
-        await msg.reply({
-            content: "check dm", 
-            ephemeral: true,
-        }).then(tmsg => {setTimeout(() => tmsg.delete(), 3000)})
     let time = Date.now();
-    await msg.user.send({
-        content: `Pong! recive time ${Date.now() - msg.createdTimestamp}ms API ${client.ws.ping}ms`, 
-        ephemeral: true,
+    await msg.reply({
+        content: `Pong! recive time ${Date.now() - msg.createdTimestamp}ms API ${client.ws.ping}ms`
     })
-    await msg.user.send({
-        content: `send time ${Date.now() - time}ms`, 
-        ephemeral: true,
+    await msg.channel.send({
+        content: `Send time ${Date.now() - time}ms`
     })
 }
