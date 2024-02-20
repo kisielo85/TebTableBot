8a0a9e1c4fdfb35048d6938e2602eae3d36d7c5f
Marcel Mikulski <100922591+Le-programer@users.noreply.github.com>
2024-02-20 12:37:21 +0100

    naprawiony clear
    uga buga nie ma spamowania

--- commands/clear.js
+++ commands/clear.js
@@ -1,8 +1,22 @@
+const { sleep } = require("../utils/simpleFunctions");
+
+let deleteing = {}
 module.exports = async ({msg, client}) => {
-    await msg.reply('clearing...');
-    const messages = await msg.channel.messages.fetch({'limit':50})
-    messages.forEach(async msg => {
-        if (msg.author.id == client.user.id)
-            await msg.delete()
-    })
+    if(!deleteing[msg.user.id]){
+        await msg.reply('clearing...');
+        
+        deleteing[msg.user.id] = await msg.channel.messages.fetch({'limit':100})
+
+        for(let a of deleteing[msg.user.id]){
+            console.log(a[1])
+            
+            if (a[1].author.id == client.user.id)
+                await a[1].delete()
+        }
+        
+        delete deleteing[msg.user.id];
+
+    }else{
+        await msg.reply("already deleting")
+    }
 }
