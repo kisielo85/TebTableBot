7390b0a10bfb3b5349711f5163f805d5dd97df49
Marcel Mikulski <100922591+Le-programer@users.noreply.github.com>
2024-02-20 12:57:30 +0100

    czybka poprawka

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,5 @@
 module.exports = async ({msg}) => {
     let rand = Math.floor(Math.random()*10)
-    console.log(rand)
     if(rand == 1){
         msg.channel.send("https://static.wikia.nocookie.net/nicos-nextbots-fanmade/images/7/7c/Uncannyclose.png/revision/latest?cb=20230629050427").then(msg => setTimeout(() => msg.delete(), 200))
     }
